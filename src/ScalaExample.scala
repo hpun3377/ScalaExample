bb2839d6d4664f42e8127461685aa2b18764a66a
sungwon.park <hpun3377@gmail.com>
2015-10-27 00:11:11 +0900

    I studied about different two variables, var and val

--- src/ScalaExample.scala
+++ src/ScalaExample.scala
@@ -1,8 +1,19 @@
 /**
  * Created by bagseong-won on 15. 10. 25..
  */
+package com.liftcode.stuff
+import scala.xml
+import scala.collection.mutable.HashMap
+import scala.collection.immutable.{TreeMap, TreeSet}
+
 object ScalaExample {
   def main (args: Array[String]) {
-    print("Hello World")
+    val s = "hello"
+
+    println(s)
+
+    var myVar : String = "Foo"
+    val myVal : String = "Foo"
+
   }
 }
